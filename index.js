3508f8f84fd6958b709b5820b23aec0b5698997a
Alan Rocha <141082245+alanrochagomes@users.noreply.github.com>
2023-11-09 14:42:32 +0000

    Criação do endpoint ´Create [POST]  /item`

--- index.js
+++ index.js
@@ -1,6 +1,12 @@
 const express = require('express')
 const app = express()
 
+// Registrar um Middleware
+// indica que todas as requisições podem receber
+// Body em JSON. A partir disso, o Express aplica
+// um JSON.parse para o conteúdo recebido
+app.use(express.json())
+
 app.get('/', function (req, res) {
   res.send('Hello World')
 })
@@ -10,6 +16,7 @@ app.get("/oi", function (req, res) {
 })
 
 const lista = ["Rick Sanchez", "Morty Smith", "Summer Smith"]
+//                0                 1              2
 // Read by ID - [GET] /item
 app.get("/item", function (req, res) {
   res.send(lista)
@@ -26,6 +33,18 @@ app.get("/item/:id", function (req, res) {
 
   // Enviamos o item como resposta do endpoint
   res.send(item)
+})
+
+// Create - [POST] /item
+app.post("/item", function (req, res) {
+  // Extraímos o nome do Body da Requisição
+  const item = req.body.nome
+
+  //adicionamos o item recebido na lista
+  lista.push(item)
+
+  // Exibimos uma mensagem de sucesso
+  res.send("item adicionado com sucesso!")
 })
 
 app.listen(3000)
